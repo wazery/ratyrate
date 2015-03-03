8d80b194a6bdf5c290268691df4155c9c42b2d1c
Shah Zaib <bc090201498@hotmail.com>
2015-03-03 16:35:06 +0500

    readonly fixes

--- lib/ratyrate/helpers.rb
+++ lib/ratyrate/helpers.rb
@@ -1,72 +1,77 @@
 module Helpers
   def rating_for(rateable_obj, dimension=nil, options={})
-    starts = if user = options[:user]
-               rating = user.ratings_given.where(rateable: rateable_obj, dimension: dimension).first
-               rating ? rating.stars : 0
-             else
-               cached_average = rateable_obj.average dimension
-               cached_average ? cached_average.avg : 0
-             end
 
-    star = options[:star] || 5
-    enable_half = options[:enable_half] || false
-    half_show = options[:half_show] || false
-    star_path = options[:star_path] || "/assets"
-    star_on = asset_path(options[:star_on] || "star-on.png")
-    star_off = asset_path(options[:star_off] || "star-off.png")
-    star_half = asset_path(options[:star_half] || "star-half.png")
-    cancel = options[:cancel] || false
-    cancel_place = options[:cancel_place] || "left"
-    cancel_hint = options[:cancel_hint] || "Cancel current rating!"
-    cancel_on = asset_path(options[:cancel_on] || "cancel-on.png")
-    cancel_off = asset_path(options[:cancel_off] || "cancel-off.png")
+    star             = options[:star] || 5
+    enable_half      = options[:enable_half] || false
+    half_show        = options[:half_show] || false
+    star_path        = options[:star_path] || "/assets"
+    star_on          = asset_path(options[:star_on] || "star-on.png")
+    star_off         = asset_path(options[:star_off] || "star-off.png")
+    star_half        = asset_path(options[:star_half] || "star-half.png")
+    cancel           = options[:cancel] || false
+    cancel_place     = options[:cancel_place] || "left"
+    cancel_hint      = options[:cancel_hint] || "Cancel current rating!"
+    cancel_on        = asset_path(options[:cancel_on] || "cancel-on.png")
+    cancel_off       = asset_path(options[:cancel_off] || "cancel-off.png")
     no_rated_message = options[:no_rated_message] || "I'am readOnly and I haven't rated yet!"
-    space = options[:space] || false
-    single = options[:single] || false
-    target = options[:target] || ''
-    target_text = options[:target_text] || ''
-    target_type = options[:target_type] || 'hint'
-    target_format = options[:target_format] || '{score}'
-    target_score = options[:target_score] || ''
-    hints = options[:hints] || ['bad', 'poor', 'regular', 'good', 'gorgeous']
+    space            = options[:space] || false
+    single           = options[:single] || false
+    target           = options[:target] || ''
+    target_text      = options[:target_text] || ''
+    target_type      = options[:target_type] || 'hint'
+    target_format    = options[:target_format] || '{score}'
+    target_score     = options[:target_score] || ''
+    hints            = options[:hints] || ['bad', 'poor', 'regular', 'good', 'gorgeous']
 
     disable_after_rate = options[:disable_after_rate] && true
     disable_after_rate = true if disable_after_rate == nil
 
-    if disable_after_rate
-      readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
-    else
-      readonly = !current_user || false
-    end
+    readonly = if options.has_key?(:read_only)
+                  options[:read_only] == true
+                elsif disable_after_rate
+                  !(current_user && rateable_obj.can_rate?(current_user, dimension))
+                else
+                  false
+                end
+
+    starts = if options[:stars]
+                options[:stars].to_s
+             elsif user  = options[:user]
+               rating = user.ratings_given.where(rateable: rateable_obj, dimension: dimension).first
+               rating ? rating.stars.to_s : 0
+             elsif dimension.present?
+               cached_average = rateable_obj.average dimension
+               cached_average ? cached_average.avg.to_s : 0
+             end
 
     content_tag :div, '', class: 'star', data: {
-        dimension: dimension,
-        rating: starts,
-        id: rateable_obj.id,
-        classname: rateable_obj.class.name,
+        dimension:          dimension,
+        rating:             starts,
+        id:                 rateable_obj.id,
+        classname:          rateable_obj.class.name,
         disable_after_rate: disable_after_rate,
-        readonly: readonly,
-        enable_half: enable_half,
-        half_show: half_show,
-        star_count: star,
-        path: star_path,
-        star_on: star_on,
-        star_off: star_off,
-        star_half: star_half,
-        cancel: cancel,
-        cancel_place: cancel_place,
-        cancel_hint: cancel_hint,
-        cancel_on: cancel_on,
-        cancel_off: cancel_off,
-        no_rated_message: no_rated_message,
-        hints: hints,
-        space: space,
-        single: single,
-        target: target,
-        target_text: target_text,
-        target_type: target_type,
-        target_format: target_format,
-        target_score: target_score
+        readonly:           readonly,
+        enable_half:        enable_half,
+        half_show:          half_show,
+        star_count:         star,
+        path:               star_path,
+        star_on:            star_on,
+        star_off:           star_off,
+        star_half:          star_half,
+        cancel:             cancel,
+        cancel_place:       cancel_place,
+        cancel_hint:        cancel_hint,
+        cancel_on:          cancel_on,
+        cancel_off:         cancel_off,
+        no_rated_message:   no_rated_message,
+        hints:              hints,
+        space:              space,
+        single:             single,
+        target:             target,
+        target_text:        target_text,
+        target_type:        target_type,
+        target_format:      target_format,
+        target_score:       target_score
     }
   end
 
